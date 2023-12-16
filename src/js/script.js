020114fdecb42c382f4b709cdca7100268366652
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2023-12-16 16:19:10 +0200

    hw-2 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+
+let name = "John";
+
+
+let admin = name;
+
+alert(admin);
+
+let planetEarth = "Earth";
+
+let userName = "Jonh";
+
+
+let mixedValue = "10" + 10;
+
+
+let variableType = typeof mixedValue;
+
+console.log("mixedValue" + variableType);
+
