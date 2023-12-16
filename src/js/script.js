65c29f244ffe169af9bc4ef95a6329be7583c712
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2023-12-16 16:33:39 +0200

    hw-3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,28 @@
 'use strict'
 
-
-let name = "John";
-
-
-let admin = name;
-
-alert(admin);
-
-let planetEarth = "Earth";
-
-let userName = "Jonh";
+let operand1 = prompt("Enter the first operand:");
+let operand2 = prompt("Enter the second operand:");
 
 
-let mixedValue = "10" + 10;
+operand1 = parseFloat(operand1);
+operand2 = parseFloat(operand2);
 
 
-let variableType = typeof mixedValue;
+if (Number.isNaN(operand1) || Number.isNaN(operand2)) {
+    console.log("Please enter valid numbers.");
+} else {
+    // Perform operations and output the results to the console
+    console.log(`Addition Result: ${operand1} + ${operand2} = ${operand1 + operand2}`);
+    console.log(`Subtraction Result: ${operand1} - ${operand2} = ${operand1 - operand2}`);
+    console.log(`Multiplication Result: ${operand1} * ${operand2} = ${operand1 * operand2}`);
 
-console.log("mixedValue" + variableType);
+
+    if (operand2 !== 0) {
+        console.log(`Division Result: ${operand1} / ${operand2} = ${operand1 / operand2}`);
+    } else {
+        console.log("Division by 0 is not possible.");
+    }
+}
+
+
 
