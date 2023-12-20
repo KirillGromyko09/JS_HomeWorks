5d32d230afb1f657f68e08eb661222ce964baa56
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2023-12-20 02:52:27 +0200

    hw-4 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const input_str1 = prompt("Enter the first string:");
+const input_str2 = prompt("Enter the second string:");
+const input_str3 = prompt("Enter the third string:");
+
+
+const concatenated_str = input_str1 + input_str2 + input_str3;
+console.log("Strings in arbitrary order:", concatenated_str);
+
+
+const five_digit_number = prompt("Enter a five-digit number:");
+const digits_list = five_digit_number.split("").map(Number);
+console.log("Digits of the five-digit number separated by space:", digits_list.join(" "));
